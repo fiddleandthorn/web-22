cfbda824616132b4e739c9203088e121ad5edb11
Billy <billydawson98@gmail.com>
2022-01-04 17:18:38 +0000

    Added Redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,16 @@ const { paginate } = require("gatsby-awesome-pagination")
 const redirects = require("./redirects.json");
 
 exports.createPages = async ({ graphql, actions }) => {
+
+  const { createRedirect } = actions;
+
+	redirects.forEach(redirect =>
+		createRedirect({
+	    fromPath: redirect.fromPath,
+	    toPath: redirect.toPath
+	  })
+	);
+
   const { createPage } = actions
 
 
