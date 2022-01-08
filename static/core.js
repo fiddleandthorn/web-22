10538c67bf043bf0ac916f603e9caa29793c5dbd
Billy <billydawson98@gmail.com>
2022-01-08 17:04:43 +0000

    Ads.txt

--- static/core.js
+++
@@ -1,7 +0,0 @@
-
-    glowCookies.start('en', {
-        style: 1,
-        analytics: 'G-FH87DE17XF',
-        facebookPixel: '990955817632355',
-        policyLink: 'https://link-to-your-policy.com'
-    });
