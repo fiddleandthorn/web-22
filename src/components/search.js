2a8640d546cf002cb110ab5fbee44fc4add6b2a6
Billy <billydawson98@gmail.com>
2022-01-10 15:32:28 +0000

    Search Edits

--- src/components/search.js
+++ src/components/search.js
@@ -34,7 +34,28 @@ const Results = connectStateResults(({ searchState, title }) =>
 );
 
 export default function Search({ indexName }) {
-    const searchClient = algoliasearch(process.env.GATSBY_ALGOLIA_APP_ID, process.env.GATSBY_ALGOLIA_SEARCH_KEY);
+    // const searchClient = algoliasearch(process.env.GATSBY_ALGOLIA_APP_ID, process.env.GATSBY_ALGOLIA_SEARCH_KEY);
+    const algoliaClient = algoliasearch(process.env.GATSBY_ALGOLIA_APP_ID, process.env.GATSBY_ALGOLIA_SEARCH_KEY);
+
+    const searchClient = {
+      ...algoliaClient,
+      search(requests) {
+        if (requests.every(({ params }) => !params.query)) {
+          return Promise.resolve({
+            results: requests.map(() => ({
+              hits: [],
+              nbHits: 0,
+              nbPages: 0,
+              page: 0,
+              processingTimeMS: 0,
+            })),
+          });
+        }
+
+        return algoliaClient.search(requests);
+      },
+    };
+
     return (
           <InstantSearch indexName={indexName} searchClient={searchClient}>
             <div className="container">
