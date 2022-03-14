3edc617aaf1ac7076fd772a8e098c8e28c29b614
Billy <billydawson98@gmail.com>
2022-03-14 20:33:08 +0000

    Major Style Alterations

--- src/pages/index.js
+++ src/pages/index.js
@@ -131,7 +131,7 @@ const IndexPage = ({ data }) => {
       </div>
 
       <div className="shape-wrapper light">
-        <svg class="shape" viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z" fill="currentColor"></path></svg>
+        <svg class="shape" viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0 48h2880V0h-720C1442.5 52 720 0 720 0H0v48z" fill="#2D361F"></path></svg>
       </div>
 
       <div className="related-plants featured-plants">
